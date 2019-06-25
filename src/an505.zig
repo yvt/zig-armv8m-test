869721ac325a618e84fcc91a41898b87fa117fa3
yvt <i@yvt.jp>
2019-06-26 00:30:11 +0900

    Add TZ MPC driver

--- src/an505.zig
+++ src/an505.zig
@@ -1,4 +1,8 @@
 const Pl011 = @import("pl011.zig").Pl011;
+const TzMpc = @import("tz_mpc.zig").TzMpc;
 
 /// UART 0 (secure) - J10 port
 pub const uart0 = Pl011.with_base(0x40200000);
+
+pub const ssram1_mpc = TzMpc.with_base(0x58007000);
+pub const iram_mpc = TzMpc.with_base(0x58009000);
---
+++ src/tz_mpc.zig
@@ -0,0 +1,131 @@
+const CTRL_SECURITY_ERROR_RESPONSE: u32 = 1 << 4;
+const CTRL_AUTOINCREMENT: u32 = 1 << 8;
+
+/// The device driver for the TrustZone Memory Protection Controller.
+///
+/// It is documented in the ARM CoreLink SIE-200 System IP for Embedded TRM
+/// (DDI 0571G):
+/// <https://developer.arm.com/products/architecture/m-profile/docs/ddi0571/g>
+/// A software implementation can be found in QEMU's `tz-mpc.c`.
+pub const TzMpc = struct {
+    base: usize,
+
+    const Self = @This();
+
+    /// Construct a `Pl011` object using the specified MMIO base address.
+    pub fn with_base(base: usize) Self {
+        return Self{ .base = base };
+    }
+
+    /// Assign an address range to Non-Secure.
+    ///
+    /// The range might be rounded to the block size the hardware is configured
+    /// with.
+    pub fn assign_range_to_non_secure(self: Self, start: u32, end: u32) void {
+        self.update_range(start, end, Masks{ 0x00000000, 0xffffffff });
+    }
+
+    /// Assign an address range to Secure.
+    ///
+    /// The range might be rounded to the block size the hardware is configured
+    /// with.
+    pub fn assign_range_to_secure(self: Self, start: u32, end: u32) void {
+        self.update_range(start, end, Masks{ 0x00000000, 0x00000000 });
+    }
+
+    pub fn set_enable_bus_error(self: Self, value: bool) void {
+        if (value) {
+            self.reg_ctrl().* |= CTRL_SECURITY_ERROR_RESPONSE;
+        } else {
+            self.reg_ctrl().* &= ~CTRL_SECURITY_ERROR_RESPONSE;
+        }
+    }
+
+    fn reg_ctrl(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base);
+    }
+
+    fn reg_blk_max(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x10);
+    }
+
+    fn reg_blk_cfg(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x14);
+    }
+
+    fn reg_blk_idx(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x18);
+    }
+
+    fn reg_blk_lut(self: Self) *volatile u32 {
+        return @intToPtr(*volatile u32, self.base + 0x1c);
+    }
+
+    // TODO: Registers related to interrupt
+
+    fn block_size_shift(self: Self) u5 {
+        return @truncate(u5, self.reg_blk_cfg().*) + 5;
+    }
+
+    fn update_lut(self: Self, masks: Masks) void {
+        const lut = self.reg_blk_lut();
+        lut.* = (lut.* & masks[0]) ^ masks[1];
+    }
+
+    fn update_range(self: Self, start: u32, end: u32, masks: Masks) void {
+        // (Silently) round to the block size used by the hardware
+        const shift = self.block_size_shift();
+        start >>= shift;
+        end >>= shift;
+
+        if (start >= end) {
+            return;
+        }
+
+        const start_group = start / 32;
+        const end_group = end / 32;
+
+        self.reg_ctrl().* &= ~CTRL_AUTOINCREMENT;
+        self.reg_blk_idx().* = start_group;
+
+        if (start_group == end_group) {
+            masks = filter_masks(masks, ones_from(start % 32) ^ ones_from(end % 32));
+            self.update_lut(masks);
+        } else {
+            var group = start_group;
+
+            if ((start % 32) != 0) {
+                const cap_masks = filter_masks(masks, ones_from(start % 32));
+                self.update_lut(cap_masks);
+
+                group += 1;
+                self.reg_blk_idx().* = group;
+            }
+
+            while (group < end_group) {
+                self.update_lut(masks);
+
+                group += 1;
+                self.reg_blk_idx().* = group;
+            }
+
+            if ((end % 32) != 0) {
+                const cap_masks = filter_masks(masks, ~ones_from(end % 32));
+                self.update_lut(cap_masks);
+            }
+        }
+    }
+};
+
+/// AND and XOR masks.
+const Masks = [2]u32;
+
+fn filter_masks(masks: Masks, filter: u32) Masks {
+    return Masks{ masks[0] & ~filter, masks[1] & filter };
+}
+
+/// Returns `0b11111000...000` where the number of trailing zeros is specified
+/// by `pos`. `pos` must be in `[0, 31]`.
+fn ones_from(pos: u32) u32 {
+    return u32(0xffffffff) << @intCast(u5, pos);
+}
