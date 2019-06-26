e4544e4989f8a9d61216e4b14d9a6f9c0b6a919d
yvt <i@yvt.jp>
2019-06-26 23:11:11 +0900

    Add wrapper and helper functions for the `TT*` instructions

--- src/drivers/arm_cmse.zig
+++ src/drivers/arm_cmse.zig
@@ -1,5 +1,6 @@
 // Suppoting functions for Cortex-M Security Extensions
 const builtin = @import("builtin");
+const assert = @import("std").debug.assert;
 
 /// Call a niladic Non-Secure function.
 pub inline fn callNs0(func: var) @typeInfo(@typeOf(func)).Fn.return_type.? {
@@ -25,6 +26,153 @@ fn innerCallNs0(func: var) @typeInfo(@typeOf(func)).Fn.return_type.? {
     );
 
     unreachable;
+}
+
+/// Generate a `TT` instruction..
+pub inline fn tt(p: var) AddressInfo {
+    return AddressInfo{
+        .value = asm volatile (
+            \\ .cpu cortex-m33
+            \\ tt %0, %1
+            : [out] "=r" (-> u32)
+            : [p] "r" (p)
+        ),
+    };
+}
+
+/// Generate a `TT` instruction with the `A` flag (alternate security mode).
+pub inline fn tta(p: var) AddressInfo {
+    return AddressInfo{
+        .value = asm volatile (
+            \\ .cpu cortex-m33
+            \\ tta %[out], %[p]
+            : [out] "=r" (-> u32)
+            : [p] "r" (p)
+        ),
+    };
+}
+
+/// Generate a `TT` instruction with the `A` flag (alternate security mode) and
+/// `T` flag (non-privileged).
+pub inline fn ttat(p: var) AddressInfo {
+    return AddressInfo{
+        .value = asm volatile (
+            \\ .cpu cortex-m33
+            \\ ttat %[out], %[p]
+            : [out] "=r" (-> u32)
+            : [p] "r" (p)
+        ),
+    };
+}
+
+/// Check Non-Secure access permissions for an object of type `ty` at `ptr`.
+///
+/// Returns `@intToPtr(*volatile ty, ptr)` if the check succeeds; otherwise,
+/// `null`.
+pub inline fn checkObject(comptime ty: type, ptr: usize, options: CheckOptions) ?*volatile ty {
+    // Check alignment
+    const alignment = @alignOf(ty);
+
+    if ((ptr & ((1 << alignment) - 1)) != 0) {
+        return null;
+    }
+
+    // Check access
+    if (checkAddressRange(ptr, @sizeOf(ty), options)) {
+        return @intToPtr(?*volatile ty, ptr);
+    } else {
+        return null;
+    }
+}
+
+/// Check Non-Secure access permissions for a slice of element type `ty`
+/// starting at `ptr`, containing `count` elements.
+///
+/// Returns a slice of type `[]volatile ty` if the check succeeds; otherwise,
+/// `null`.
+pub inline fn checkSlice(comptime ty: type, ptr: usize, count: usize, options: CheckOptions) ?[]volatile ty {
+    // Check alignment
+    const alignment = @alignOf(ty);
+
+    if ((ptr & ((1 << alignment) - 1)) != 0) {
+        return null;
+    }
+
+    // Check size
+    var size: usize = undefined;
+    if (!@mulWithOverflow(usize, count, @sizeOf(ty), &size)) {
+        return null;
+    }
+
+    // Check access
+    if (checkAddressRange(ptr, size, options)) {
+        return @intToPtr([*]volatile ty, ptr)[0..size];
+    } else {
+        return null;
+    }
+}
+
+/// Check Non-Secure access permissions for the specified address range.
+///
+/// Returns `false` if the check fail; otherwise, `true`.
+///
+/// This roughly follows the address range check intrinsic described in:
+/// “ARM®v8-M Security Extensions: Requirements on Development Tools”
+pub inline fn checkAddressRange(ptr: var, size: usize, options: CheckOptions) bool {
+    const start = if (@typeOf(ptr) == usize) ptr else @ptrToInt(ptr);
+    var end: usize = start;
+
+    if (size > 0 and !@addWithOverflow(usize, start, size - 1, &end)) {
+        // The check should fail if the address range wraps around
+        return false;
+    }
+
+    const info1 = if (options.unpriv) ttat(start) else tta(start);
+    const info2 = if (size > 1)
+        (if (options.unpriv) ttat(start) else tta(start))
+    else
+        (info1);
+
+    // The chcek should fail if the range crosses any SAU/IDAU/MPU region
+    // boundary
+    if (info1.value != info2.value) {
+        return false;
+    }
+
+    return if (options.readwrite)
+        (info1.flags.nonsecure_readwrite_ok)
+    else
+        (info1.flags.nonsecure_read_ok);
+}
+
+pub const CheckOptions = struct {
+    /// Checks if the permissions have the `readwrite_ok` field set.
+    readwrite: bool = false,
+
+    /// Retrieves the unprivileged mode access permissions.
+    unpriv: bool = false,
+};
+
+/// The address information returned by a `TT` instruction.
+pub const AddressInfo = packed union {
+    flags: packed struct {
+        mpu_region: u8,
+        sau_region: u8,
+        mpu_region_valid: bool,
+        sau_region_valid: bool,
+        read_ok: bool,
+        readwrite_ok: bool,
+        nonsecure_read_ok: bool,
+        nonsecure_readwrite_ok: bool,
+        secure: bool,
+        idau_region_valid: bool,
+        idau_region: u8,
+    },
+    value: u32,
+};
+
+test "AddressInfo is word-sized" {
+    assert(@sizeOf(AddressInfo) == 4);
 }
 
 /// Export a Non-Secure-callable function.
