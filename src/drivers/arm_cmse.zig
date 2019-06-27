3a762c42dd4e8a738c32295a8dc567d9362c25ff
yvt <i@yvt.jp>
2019-06-28 00:07:29 +0900

    zig fmt

--- src/drivers/arm_cmse.zig
+++ src/drivers/arm_cmse.zig
@@ -44,7 +44,7 @@ pub fn nonSecureCall(func: var, r0: usize, r1: usize, r2: usize, r3: usize) usiz
           [r1] "{r1}" (r1),
           [r2] "{r2}" (r2),
           [r3] "{r3}" (r3),
-          [func] "{r4}" (target & ~usize(1)),
+          [func] "{r4}" (target & ~usize(1))
         : "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r8", "r9", "r10", "r11", "r12"
     );
 }
