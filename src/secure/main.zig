6ce631790e8856f3a9db3967ecb5c98c009e1b79
yvt <i@yvt.jp>
2019-06-28 16:58:01 +0900

    Remove unnecessary `\x08` output
    Fixes output discrepancy between terminal programs.

--- src/secure/main.zig
+++ src/secure/main.zig
@@ -115,7 +115,7 @@ var counter: u8 = 0;
 
 extern fn handleSysTick() void {
     counter +%= 1;
-    an505.uart0.print("\r\x08{}", "|\\-/"[counter % 4 ..][0..1]);
+    an505.uart0.print("\r{}", "|\\-/"[counter % 4 ..][0..1]);
 }
 
 /// Not a function, actually, but suppresses type error
